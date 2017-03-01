0f02ab0f433adf340ef61fd5a93959260f71e786
AppFeel <appfeel@users.noreply.github.com>
2017-03-01 22:33:43 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+require('babel-polyfill');
+require('babel-register')({
+    presets: [
+        ['env', {
+            targets: {
+                node: 'current',
+            },
+        }],
+        'stage-3',
+    ],
+});
+require('./router.js');
